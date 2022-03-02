7192eab21d38e5b38cbf258c50e2acb6838c4443
zhaoxh10 <zhaoxh10@lenovo.com>
2022-03-02 18:35:30 +0800

    Updates

---
+++ precache-manifest.5f0f4385ea97b170491d8fe35f120972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9412c56bb412e1155f6afc8d931ed2",
+    "url": "/wufang95/wufang95.github.io/index.html"
+  },
+  {
+    "revision": "17cee18626e7f1e9f474",
+    "url": "/wufang95/wufang95.github.io/static/js/2.589adcbe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wufang95/wufang95.github.io/static/js/2.589adcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b60f2e28fba177c55d09",
+    "url": "/wufang95/wufang95.github.io/static/js/main.f4b955a8.chunk.js"
+  },
+  {
+    "revision": "5410e1cb552cca885f20",
+    "url": "/wufang95/wufang95.github.io/static/js/runtime-main.5ed9d9be.js"
+  }
+]);
