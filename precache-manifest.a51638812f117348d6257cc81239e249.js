8aa9cf3b5025def98c2203fa28641353abc9d535
zhaoxh10 <zhaoxh10@lenovo.com>
2022-03-18 17:44:30 +0800

    Updates

--- precache-manifest.a51638812f117348d6257cc81239e249.js
+++ precache-manifest.a51638812f117348d6257cc81239e249.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e14c07d7fd1902ca81771bcfd01a6e0a",
+    "revision": "6f902d01930c6b3218306d54cba5bddf",
     "url": "/index.html"
   },
   {
-    "revision": "17cee18626e7f1e9f474",
-    "url": "/static/js/2.589adcbe.chunk.js"
+    "revision": "d2818e1b05ddbafa0851",
+    "url": "/static/js/2.e299bc1c.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/static/js/2.589adcbe.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e299bc1c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a455f54d1245e887ce9",
-    "url": "/static/js/main.f6e84455.chunk.js"
+    "revision": "8900b9c7b15a1695170d",
+    "url": "/static/js/main.6d91cbae.chunk.js"
   },
   {
     "revision": "f52ba179432c3be28857",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f456a158db63013fe56f7899240dca65.js"
+  "/precache-manifest.a51638812f117348d6257cc81239e249.js"
 );
 
 self.addEventListener('message', (event) => {
